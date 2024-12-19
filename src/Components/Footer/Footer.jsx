7f200387f0327ee8f8bb514bd509b66ec336e657
panduranga07 <panduvallabhuni09@gmail.com>
2024-12-19 17:27:41 +0530

    Adding Swamy

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
             </div>
         </div>
         <hr />
-        <p className="footer-copyright">Copyright 2024 © Tomato.com - All Rights Reserved by PANDURANGA.</p>
+        <p className="footer-copyright">Copyright 2024 © Tomato.com - All Rights Reserved by PANDURANGA SWAMY.</p>
     </div>
   )
 }
